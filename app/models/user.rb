9c07e48b5cc2927f9bec41b2778422a292c338a0
sokomheng <sokomheng89@gmail.com>
2017-03-22 03:26:37 +0700

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+  include Nameable
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  validates :first_name, length: { in: 2..24 }, presence: true,
+            format: { with: RegexConstants::Letters::AND_NUMBERS,
+                      message: 'Special letters are not allowed to input' }
+  validates :last_name, length: { in: 2..24 }, presence: true,
+            format: { with: RegexConstants::Words::AND_SPECIAL,
+                      message: 'Special letters are not allowed to input' }
+
+end
