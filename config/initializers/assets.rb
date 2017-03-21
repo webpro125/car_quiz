ff27adde53d3b169389e0bb8018cfd90850ce2d3
sokomheng <sokomheng89@gmail.com>
2017-03-22 06:20:18 +0700

    precompile files are added for devise controllers

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,5 +10,8 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile +=  [
     'pages.css',
+    'users/login.scss',
+    'users/password.scss',
+    'users/registrations.scss',
     'pages.js'
 ]
