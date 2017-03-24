f36dc4a509a74c13147fc3fc22077d2b62150ae5
sokomheng <sokomheng89@gmail.com>
2017-03-25 00:17:02 +0700

    change time zone

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,6 @@ module Carquiz
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.active_record.default_timezone = :utc
   end
 end
